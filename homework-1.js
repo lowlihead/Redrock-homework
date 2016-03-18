84fb2878ec3ecee191ef7ed0296ae5cf0066e2bd
lowlihead <qinyinze@outlook.com>
2016-03-18 09:22:26 +0800

    homework-1

---
+++ homework-1.js
@@ -0,0 +1,10 @@
+function Person(name,id){
+  this.name=name;
+  this.id=id;
+}
+var x = new Person("mark","208952");
+var y = new Person("tyee","565942");
+console.log(x.id);
+console.log(x.name);
+console.log(y.name);
+console.log(y.id);
